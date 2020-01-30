3aa6fdc299cbd47c37883d24d78d74f86da8052c
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-01-29 19:22:22 -0500

    fixed tests to compile

--- test-array.cpp
+++ test-array.cpp
@@ -2,44 +2,31 @@
 #include "array.h"
 #include <assert.h>
 
-class Integer : public Object
-{
-    public:
-        int val;
-        Integer(int ii)
-        {
-            val = ii;
-        }
-};
-
 int
 main(int argc, char **argv)
 {
     int ii = 0;
-    Array aa;
+    IntArray* aa = new IntArray();
     
     // test for append
     for(; ii < 100; ii++)
     {
-        Integer *jj = new Integer(ii);
-        aa.append(jj);
+        aa->append(ii);
     }
 
     // test for length
-    assert(aa.length() == 100);
-    Array bb;
+    assert(aa->length() == 100);
+    IntArray* bb = new IntArray();
 
 
 
-    assert(aa.equals(bb) == 0);
+    assert(!aa->equals(bb));
     ii = 0;
     for(; ii < 100; ii++)
     {
-        Integer *dd = new Integer(ii);
-        bb.append(dd);
+        bb->append(ii);
     }
-    assert(aa.equals(bb) == 0);
-    assert(aa.equals(aa) == 1);
+    assert(aa->equals(bb));
 
     puts("all tests passed");
     return 0;
